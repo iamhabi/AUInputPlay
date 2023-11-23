2997fe43aa0d726678562b38cf6369b305f68ba0
iamhabi <habiam@pm.me>
2023-11-23 21:19:57 +0900

    destroy aggregate device on terminate

--- AUInputPlay/AUInputPlayApp.swift
+++ AUInputPlay/AUInputPlayApp.swift
@@ -9,12 +9,27 @@ import CoreMIDI
 import SwiftUI
 
 @main
-struct AUInputPlayApp: App {
-    @ObservedObject private var hostModel = AudioUnitHostModel()
+class AUInputPlayApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
+    @ObservedObject var hostModel: AudioUnitHostModel = AudioUnitHostModel()
+    
+    required init() {
+        NotificationCenter.default.addObserver(forName: NSApplication.willTerminateNotification, object: nil, queue: .main) { _ in
+            self.hostModel.stop()
+            self.hostModel.destroyAggregateDevice()
+        }
+    }
 
     var body: some Scene {
         WindowGroup {
             ContentView(hostModel: hostModel)
         }
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
     }
 }
